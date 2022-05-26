e448c3282a3d2c10d429ab58fccaf7e43656ba84
etg <eidano_x@qq.com>
2022-05-26 14:28:52 +0800

    add skeleton

--- main.lua
+++ main.lua
@@ -1,2 +1,23 @@
 local name = "Valio"
 print(name)
+
+-- --------------------------- function ----------------------------
+local function myLog(str)
+    local var = str
+    print(var .. " is logged")
+    return var
+end
+
+myLog(name)
+
+-- TODO: following the notes and try all function features
+
+
+-- --------------------------- variable scope --------------------------
+-- TODO: following the notes and try variable scopes
+
+
+
+
+-- --------------------------- table ----------------------------
+-- TODO: soul of the lua language. following the notes and practice all table features
