2fe5cbb57bca905cfc18464e8a39d6dfb5a4602b
etg <eidano_x@qq.com>
2022-05-30 14:05:54 +0800

    add function and variable scope

--- main.lua
+++ main.lua
@@ -2,19 +2,64 @@ local name = "Valio"
 print(name)
 
 -- --------------------------- function ----------------------------
-local function myLog(str)
-    local var = str
-    print(var .. " is logged")
-    return var
-end
+-- ---------- variable scope -----------
+-- local function myLog(str)
+--     local var = str
+--     local name = "Sisu"
+--     -- local var = name
+--     -- print(var .. " is logged")
+--     -- return var
+--     return function ()
+--        print(name)
+--        print(var)
+--     end
+-- end
 
-myLog(name)
-
--- TODO: following the notes and try all function features
+-- myLog(name)()
 
 
--- --------------------------- variable scope --------------------------
--- TODO: following the notes and try variable scopes
+-- ---------- function def and return value ----------
+-- 1. table.unpack
+-- local function foo0() end
+-- local foo1 = function ()
+--     return 'a'
+-- end
+-- local function foo2()
+--     return 'a','b'
+-- end
+
+-- print(foo2())
+-- print(foo2(),1)
+-- -- print(table.unpack({1,2,3}))
+-- print(table.unpack{1,2,3})
+
+
+-- ---------- input arguments -----------
+-- 1. table.pack
+-- 2. select
+-- local goo = function (var1,var2,...)
+--     print(var1,var2)
+--     print(...)
+-- end
+
+-- -- goo()
+-- -- goo(1,2)
+-- goo(1,2,3,4,5)
+-- foo = {1,2,nil,nil}
+-- foo1 = table.pack(1,2,nil,nil)
+-- print(#foo)
+-- print(foo1.n)
+
+-- print(select(1,"a","b","c"))
+-- print(select(2,"a","b","c"))
+-- print(select(3,"a","b","c"))
+-- print(select("#","a","b","c"))
+
+-- TODO: closure usecase examples
+-- ---------- closure usecases ------------
+
+
+
 
 
 
