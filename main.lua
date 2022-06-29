592b2f66dd5adf2d2ca7c04fee010d6630b4485c
etg <eidano_x@qq.com>
2022-06-29 15:27:02 +0800

    add table code

--- main.lua
+++ main.lua
@@ -42,8 +42,8 @@ print(name)
 --     print(...)
 -- end
 
--- -- goo()
--- -- goo(1,2)
+-- goo(1,2)
+-- goo(1,2,nil,nil)
 -- goo(1,2,3,4,5)
 -- foo = {1,2,nil,nil}
 -- foo1 = table.pack(1,2,nil,nil)
@@ -66,16 +66,17 @@ print(name)
 
 -- --------------------------- table ----------------------------
 -- 1. foo.name = foo["name"] foo["10"] works but foo.10 never work!!
-foo = {"Valio","Sisu",name="Valio",}
+local foo = {"Valio","Sisu",name="Valio",}
 foo.name = "Simu"
 foo["10"] = "None"
 print(foo[1],foo[3],foo["name"],foo[10],foo["10"])
 
-
-
-
-
-
+print(table.concat(foo,'!!'))
+print(#foo)
+table.insert(foo,2,"what")
+for i = 1, #foo do
+    print(foo[i])
+end
 
 
 
